0cec032590fc5a03467f62af1a6c9736dff4186d
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2026-01-28 12:53:15 +0530

    Enhance home content and styling for DevOps training
    Updated text styles and content in the home section, including changes to headings and added a new section for real-time DevOps projects.

--- app.py
+++ app.py
@@ -99,10 +99,10 @@ def home():
     home_content = """
     <div class="card">
         <h1 style="color:#ffcc00;">Multi-Cloud DevOps Training</h1>
-        <h2>By Veera Sir – Naresh IT</h2>
+        <h2 style="color:#f87171;">By Veera Sir – Naresh IT</h2>
 
         <p>
-            Welcome to the <b>industry-oriented Multi-Cloud DevOps program</b> designed by
+            Welcome to the <b>industry-oriented Multi-Cloud DevOps Training</b> designed by
             <b>Veera Sir</b> at <b>Naresh IT</b>.
         </p>
 
@@ -111,6 +111,41 @@ def home():
             and end-to-end CI/CD pipelines.
         </p>
     </div>
+
+    <div class="card" style="background:#1a1a2e; color:#ffcc66; padding:20px; line-height:1.8; border-radius:12px; box-shadow: 0 4px 12px rgba(0,0,0,0.5);">
+    <h2 style="color:#ffaa00; font-size:28px; margin-bottom:15px;">Real-Time DevOps Projects By veera Sir NareshIt</h2>
+    <ul style="list-style-type: square; padding-left: 20px; font-size:16px;">
+        <li>Multi-Tier Full-Stack Node.js Application Deployed on AWS Using 20+ Services with High Availability, Scalability, Networking, Security, and Monitoring</li>
+        <li>Cloud Deployment of a Scalable, Resilient Full-Stack Python Application on AWS EC2 with Disaster Recovery Strategy</li>
+        <li>Cloud-Native Deployment of a Full-Stack Java Spring Boot Application on Azure with High Availability and Disaster Recovery Strategy</li>
+        <li>Cloud-Native Deployment of a Full-Stack Node.js Application on GCP with High Availability, Scalability, Networking, Security</li>
+        <li>Automated Database Migration from EC2-Hosted MySQL to Amazon RDS Using AWS Database Migration Service (DMS) with Minimal Downtime and High Availability</li>
+        <li>End-to-End Application Migration from On-Premises Infrastructure to AWS Cloud with High Availability, Scalability, and Security Best Practices</li>
+        <li>Optimized Static Web Hosting Architecture on AWS S3 with CDN and Custom Domain</li>
+        <li>Implementing Automated Backup Strategies in AWS Cloud Using Serverless Lambda Functions</li>
+        <li>End-to-End CDN Optimization: Configuring CloudFront for Low Latency and Automating Cache Invalidation Using Lambda</li>
+        <li>Cloud-Native Full-Stack Application Deployment on AWS ECS Fargate with CI/CD, Auto Scaling, and Secure Networking</li>
+        <li>Unified Infrastructure Monitoring with Prometheus and Grafana: Centralized Dashboard for Multi-Server Observability</li>
+        <li>Centralized Logging with Amazon CloudWatch and Cost-Optimized Log Archival to S3 Using AWS Lambda serverless</li>
+        <li>End-to-End Automation of Multi-Tier AWS, Azure and GCP Cloud Infrastructure Using Terraform with Complete Networking, Security, and Deployment Layers</li>
+        <li>End-to-End Automation of Java Maven Application Deployment on Apache Tomcat Using CI/CD Practices</li>
+        <li>End-to-End Cloud-Native CI/CD Pipeline for Node.js Microservices with Jenkins and Kubernetes</li>
+        <li>End-to-End DevOps Automation for Node.js Microservices with GitLab CI/CD and Kubernetes (EKS)</li>
+        <li>End-to-End CI/CD Pipeline for Java Applications Using Azure DevOps, Docker, and Kubernetes AKS</li>
+        <li>End-to-End CI/CD Pipeline (GitLab CI/CD) for Java Applications Using Azure DevOps, Docker, and Kubernetes GKE</li>
+        <li>Full-Stack Microservices Application (Python, Java, Node.js) Deployment on AWS Using CI/CD, GitOps, Helm, and Centralized Monitoring with Prometheus, Grafana, and the EFK Stack</li>
+        <li>End-to-End Production-Grade EKS Cluster Deployment on AWS, Azure, and GCP Using Terraform with High Availability, Scalability, and Secure Networking</li>
+        <li>Fully Managed Serverless Full-Stack Deployment Using AWS Services (Lambda, API Gateway, DynamoDB)</li>
+        <li>End-to-End Patch Management and Monitoring Configuration Using Ansible</li>
+        <li>Containerized Full-Stack Application Deployment Using Docker & Docker Compose</li>
+        <li>Centralized Application Log Monitoring and Visualization on Amazon EKS Using the ELK Stack (Elasticsearch, Logstash, Kibana) for Enhanced Observability</li>
+        <li>Scalable Multi-Application Deployment on EC2 Using ALB Path-Based Routing</li>
+        <li>Enterprise Identity and Access Management Using AWS SSO</li>
+        <li>End-to-End CI/CD for Microservices on Amazon EKS Using Argo CD and GitHub Actions</li>
+        <li>End-to-End Kubernetes Application Deployment Using Ingress Controllers for High Availability, Load Balancing, and Traffic Management</li>
+    </ul>
+</div>
+
 
     <div class="card">
         <h2 style="color:#00e5ff;">Why Multi-Cloud DevOps?</h2>
@@ -148,7 +183,7 @@ def home():
     <div class="card">
         <h2 style="color:#ff6699;">Why Naresh IT & Veera Sir?</h2>
         <ul>
-            <li>20+ years excellence</li>
+            <li>12+ years excellence</li>
             <li>Real-time trainer</li>
             <li>Interview focused</li>
             <li>Mock interviews</li>
