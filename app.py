c0871e1d023434b203f1f62b294793c5e0045841
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2026-01-28 10:56:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import *
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Welcome to Jenkins Tutorials<h1/>"
+    return "<h1>Welcome to Jenkins Tutorials</h1>"
 
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0'
+# ❌ DO NOT use app.run() in Azure
